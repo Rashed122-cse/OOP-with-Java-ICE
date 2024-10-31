1a1f64d541f1aaa67a1fcd3ec9352d228d3891c5
MD.RASHEDUZZAMAN <rashed99.cse@gmail.com>
2024-10-31 21:56:23 +0600

    number chek with oop

--- positiveNegative.java
+++ positiveNegative.java
@@ -1,16 +1,31 @@
 import java.util.Scanner;
 
+class Number {
+    static int number;
+
+    public Number(int n) {
+       this.number = n;
+    }
+
+
+    public static void displayNumber() {
+        if (number > 0) {
+            System.out.println("number is positive :" );
+        } else if (number <0) {
+            System.out.println("number is negative : " );
+        }else{
+            System.out.println("enter number is :" + number );
+        }
+    }
+}
+
 public class positiveNegative {
     public static void main(String[] args) {
-        Scanner num = new Scanner(System.in);
-        System.out.print("Enter number :");
+        Scanner sc = new Scanner(System.in) ;
+        System.out.print("enter number ");
+        Number.number = sc.nextInt();
+        Number.displayNumber();
 
-        int Number = num.nextInt() ;
 
-        if (Number > 0 ){
-            System.out.println("Number is positive ");
-        }else{
-            System.out.println("number is negative ");
-        }
     }
 }
