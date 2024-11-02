d286f09d674add02cc24a7c1b01461143f3bafb1
MD.RASHEDUZZAMAN <rashed99.cse@gmail.com>
2024-11-03 01:19:24 +0600

    oop

---
+++ naturalNumber.java
@@ -0,0 +1,14 @@
+
+public class naturalNumber {
+    public static void main(String[] args) {
+    display();
+    }
+
+        public static void display(){
+        int number;
+        for( number=1 ; number < 10 ; number++){
+                System.out.print(number + ",");
+            }
+            System.out.println(number);
+        }
+}
