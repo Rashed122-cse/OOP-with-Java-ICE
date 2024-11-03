1076ba824e8e6e3f0c041e200505248e58881303
MD.RASHEDUZZAMAN <rashed99.cse@gmail.com>
2024-11-03 17:25:28 +0600

    oop

--- positiveNegative.java
+++ positiveNegative.java
@@ -1,14 +1,10 @@
 import java.util.Scanner;
 
 class Number {
-    int number; // Non-private instance variable
-
-    // Constructor to initialize the number
+    int number;
     public Number(int number) {
         this.number = number;
     }
-
-    // Method to display whether the number is positive, negative, or zero
     public void displayNumber() {
         if (number > 0) {
             System.out.println("Number is positive: " + number);
@@ -19,16 +15,11 @@ class Number {
         }
     }
 }
-
 public class positiveNegative {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-
-        // Create an instance of Number with user input
-        Number numberInstance = new Number(sc.nextInt());
-
-        // Display whether the number is positive, negative, or zero
-        numberInstance.displayNumber();
+        Number number = new Number(sc.nextInt());
+        number.displayNumber();
     }
 }
